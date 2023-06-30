dd45ba665067850a1e2b559dd67c88e3c3775784
mercury011 <m.latinis@gmail.com>
2023-06-30 20:36:06 +0200

    Collecting include lines

--- files_collector.py
+++ files_collector.py
@@ -1,11 +1,15 @@
 from sys import argv
 
 
+def collect_include_lines(c_file_path):
+    with open(argv[1]) as c_file:
+        return [line for line in c_file if line.startswith("#include")]
+
+
 if len(argv) != 2:
     print("Usage:")
     print("python3 files_collector.py ./relative/path/to/c/file")
     exit(1)
 
-with open(argv[1]) as c_file:
-    for line in c_file:
-        print(line)
+include_lines = collect_include_lines(argv[1])
+print(include_lines)
