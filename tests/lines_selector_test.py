2e3fbe870f87d39ceb4d8974c11962bbddc8f090
mercury011 <m.latinis@gmail.com>
2023-06-30 23:35:05 +0200

    Add testsfor system header includes

--- tests/lines_selector_test.py
+++ tests/lines_selector_test.py
@@ -3,9 +3,18 @@ import unittest
 from lines_selector import collect_include_lines
 
 
-class LinesSelector(unittest.TestCase):
-    def test_when_input_empty_collects_no_lines(self):
+class CollectIncludeLines(unittest.TestCase):
+    def test_emptyInput_returnsEmptyOutput(self):
         self.assertFalse(collect_include_lines([]))
+
+    def test_inputWithoutIncludeLines_returnsEmptyOutput(self):
+        c_code_line = "int main() { return 0; }"
+        self.assertFalse(collect_include_lines([c_code_line]))
+
+    def test_inputHasSystemHeader_returnsSystemHeader(self):
+        system_header = "#include <stdio.h>"
+        self.assertEquals(collect_include_lines(
+            [system_header]), [system_header])
 
 
 unittest.main()
