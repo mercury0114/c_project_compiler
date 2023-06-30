13f72e0ad379b609943d07ab4edf2ee6566988d9
mercury011 <m.latinis@gmail.com>
2023-06-30 20:27:20 +0200

    Opening a c file to read

--- files_collector.py
+++ files_collector.py
@@ -5,3 +5,7 @@ if len(argv) != 2:
     print("Usage:")
     print("python3 files_collector.py ./relative/path/to/c/file")
     exit(1)
+
+with open(argv[1]) as c_file:
+    for line in c_file:
+        print(line)
