a9b5e57c25a3d8a9872ce080f267117349b08857
mercury011 <m.latinis@gmail.com>
2023-07-01 16:29:06 +0200

    Make get_c_files_to_compile

--- c_files_collector.py
+++ c_files_collector.py
@@ -1,6 +1,29 @@
+from functools import reduce
 from headers_collector import get_user_headers_paths
+from os.path import exists, join
 
 
-def get_c_files_to_compile(c_program_path):
-    headers = get_user_headers_paths(c_program_path)
-    return [header[:-1] + "c" for header in headers]
+class CFilesCollector:
+    def __init__(self, project_dir):
+        self.project_dir = project_dir
+        self.checked_h_paths = set()
+        self.collected_c_paths = set()
+
+    def get_more_c_paths(self, c_program_path):
+        h_paths = get_user_headers_paths(join(self.project_dir,
+                                              c_program_path))
+        unchecked_h = [h for h in h_paths if h not in self.checked_h_paths]
+        c_paths = [h[:-1] + "c" for h in unchecked_h]
+        return list(filter(lambda c: c not in self.collected_c_paths and
+                           exists(join(self.project_dir, c)), c_paths))
+
+    def collect(self, c_path):
+        unchecked_c_paths = self.get_more_c_paths(c_path)
+        any(map(self.collected_c_paths.add, unchecked_c_paths))
+        any(map(self.collect, unchecked_c_paths))
+
+
+def get_c_files_to_compile(project_dir, c_program_path):
+    collector = CFilesCollector(project_dir)
+    collector.collect(c_program_path)
+    return sorted(list(collector.collected_c_paths))
--- tests/c_files_collector_test.py
+++ tests/c_files_collector_test.py
@@ -5,24 +5,28 @@ from c_files_collector import get_c_files_to_compile
 
 class GetCFilesToCompile(unittest.TestCase):
     def test_cProgramHasNoDependencies_returnsEmptyList(self):
-        self.assertFalse(get_c_files_to_compile(
-            "tests/data/project0/program_without_dependencies.c"))
+        self.assertFalse(get_c_files_to_compile("tests/data/project0",
+                                                "program_without_dependencies.c"))
 
     def test_cProgramHasNoUserDependencies_returnsEmptyList(self):
         self.assertFalse(get_c_files_to_compile(
-            "tests/data/project0/program_without_user_dependencies.c"))
+            "tests/data/project0", "program_without_user_dependencies.c"))
 
-    def test_cProgramHasOneUserDependency_returnsCDepenencyPath(self):
+    def test_cProgramHasOneUserDependency_returnsCDependencyPath(self):
         self.assertEqual(get_c_files_to_compile(
-            "tests/data/project1/main.c"), ["library.c"])
+            "tests/data/project1", "main.c"), ["library.c"])
 
     def test_cProgramHasTwoUserDependencies_returnsTwoPaths(self):
         self.assertEqual(get_c_files_to_compile(
-            "tests/data/project2/main.c"), ["library1.c", "library2.c"])
+            "tests/data/project2", "main.c"), ["library1.c", "library2.c"])
 
     def test_cProgramDependsOnTwoHeadersAndOneCFile_returnsCFilePath(self):
         self.assertEqual(get_c_files_to_compile(
-            "tests/data/project3/main.c"), ["function.c"])
+            "tests/data/project3", "main.c"), ["function.c"])
+
+    def test_cProgramHasIndirectDependencies_returnsAllCPaths(self):
+        self.assertEqual(get_c_files_to_compile(
+            "tests/data/project4", "main.c"), ["function.c", "structure.c"])
 
 
 unittest.main()
