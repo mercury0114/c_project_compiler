44d62c15b408fc0dd154913e5f646277392d59ab
mercury011 <m.latinis@gmail.com>
2023-07-01 11:30:14 +0200

    Add test_cProgramHasNoUserDependencies

--- tests/c_files_collector_test.py
+++ tests/c_files_collector_test.py
@@ -6,11 +6,11 @@ from c_files_collector import get_c_files_to_compile
 class GetCFilesToCompile(unittest.TestCase):
     def test_cProgramHasNoDependencies_returnsEmptyList(self):
         self.assertFalse(get_c_files_to_compile(
-            "data/program_without_dependencies.c"))
+            "data/project0/program_without_dependencies.c"))
 
     def test_cProgramHasNoUserDependencies_returnsEmptyList(self):
         self.assertFalse(get_c_files_to_compile(
-            "data/program_without_user_dependencies.c"))
+            "data/project0/program_without_user_dependencies.c"))
 
 
 unittest.main()
