71c884221659f0d9ff773f11ba473586372e1463
Marius Latinis <mariusl@google.com>
2023-07-10 16:56:15 +0000

    Updating run_c_program.sh to check for input

--- run_c_program.sh
+++ run_c_program.sh
@@ -1,8 +1,13 @@
 set -e
+if [ -z $1 ]; then
+  echo "Please enter relative path (from the current dir) to your_program.c"
+  exit 1
+fi
 dr=$(dirname "$0")
 c_program_path=$PWD/$1
 project_root_dir=$(python3 -B $dr/project_directory_finder.py $c_program_path)
 c_paths=$(python3 -B $dr/c_paths_collector.py $c_program_path)
 cd $project_root_dir 
+echo "$c_paths"
 gcc -o /tmp/binary -I./ $c_paths
 chmod +x /tmp/binary ; /tmp/binary
