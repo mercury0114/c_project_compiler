7f2791165672c93358d28a76a92a74d5bac71c0b
Marius Latinis <mariusl@google.com>
2023-07-10 16:58:17 +0000

    Remove dumb line

--- run_c_program.sh
+++ run_c_program.sh
@@ -8,6 +8,5 @@ c_program_path=$PWD/$1
 project_root_dir=$(python3 -B $dr/project_directory_finder.py $c_program_path)
 c_paths=$(python3 -B $dr/c_paths_collector.py $c_program_path)
 cd $project_root_dir 
-echo "$c_paths"
 gcc -o /tmp/binary -I./ $c_paths
 chmod +x /tmp/binary ; /tmp/binary
