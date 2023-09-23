7b0ce97310dc4b2dd73c814e9e3b6dc98b5f7bf7
mercury011 <m.latinis@gmail.com>
2023-09-23 14:21:11 +0200

    Add warnings

--- run_c_program.sh
+++ run_c_program.sh
@@ -4,5 +4,5 @@ c_program_path=$PWD/$1
 project_root_dir=$(python3 -B $dr/project_directory_finder.py $c_program_path)
 c_paths=$(python3 -B $dr/c_paths_collector.py $c_program_path)
 cd $project_root_dir 
-gcc -o /tmp/binary -I./ $c_paths
+gcc -Wall -Wextra -g -o /tmp/binary -I./ $c_paths
 chmod +x /tmp/binary ; /tmp/binary
