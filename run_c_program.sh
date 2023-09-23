0851f3b33d262410feb269e44ca11161188e0d0b
mercury011 <m.latinis@gmail.com>
2023-09-23 14:54:31 +0200

    Improve local variables names in run_c_program.sh

--- run_c_program.sh
+++ run_c_program.sh
@@ -1,12 +1,18 @@
 set -e
 if [ -z $1 ]; then
-  echo "Please enter relative path (from the current dir) to your_program.c"
+  echo "Please enter relative path (from the current dir) to your_binary.c"
   exit 1
 fi
-dr=$(dirname "$0")
-c_program_path=$PWD/$1
-project_root_dir=$(python3 -B $dr/project_directory_finder.py $c_program_path)
-c_paths=$(python3 -B $dr/c_paths_collector.py $c_program_path)
+c_binary_full_path=$PWD/$1
+
+this_shell_script_full_path=$0
+compiler_dir=$(dirname "$this_shell_script_full_path")
+
+cd $compiler_dir
+project_root_dir=$(python3 -B project_directory_finder.py $c_binary_full_path)
+c_paths=$(python3 -B c_paths_collector.py $c_binary_full_path)
+
 cd $project_root_dir 
 gcc -Wall -Wextra -g -o /tmp/binary -I./ $c_paths
-chmod +x /tmp/binary ; /tmp/binary "${@:2}"
+binary_command_line_arguments=${@:2}
+chmod +x /tmp/binary ; /tmp/binary $binary_command_line_arguments
